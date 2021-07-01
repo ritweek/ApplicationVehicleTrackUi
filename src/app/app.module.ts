338edbd0faba33c9e8643ff9f8c5a083511b9b33
Ritweek Ranjan <ritweek.ranjan@pwc.com>
2021-07-01 16:27:54 +0530

    Vehicle tracking UI initial check-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 
 import { AppComponent } from './app.component';
+import { AuthGuardService } from './guards/auth-guard.service';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptor/http.interceptor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomePageComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: HomePageComponent, canActivate: [AuthGuardService]}, // only accessible if authorised
+      {path: '**', component: LoginComponent}
+    ]),
+    SocialLoginModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true, //keeps the user signed in
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('329603523313-b1q9jch28d6h12v3pf2hp9vk6fac90ik.apps.googleusercontent.com') // your client id
+        }
+      ]
+    }
+  }, AuthGuardService,
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+
+  user: SocialUser | null;
+  hasApiAccess = false;
+
+  constructor(private router: Router,
+    public socialAuthServive: SocialAuthService, private http: HttpClient) {
+
+      this.user = null;
+	this.socialAuthServive.authState.subscribe((user: SocialUser) => {
+	  console.log(user);
+	  if (user) {
+		this.http.post<any>('https://localhost:5001/user/authenticate', { idToken: user.idToken }).subscribe((authToken: any) => {
+            console.log(authToken);
+            window.localStorage.setItem('bearer', authToken.authToken);
+			
+			this.http.get<any>('https://localhost:5001/weatherforecast').subscribe((data: any) => {
+			this.hasApiAccess = true;
+			})		
+        })		  
+	  }
+	  this.user = user;
+	});
+}
+
+logout(): void {
+this.socialAuthServive.signOut().then(() => this.router.navigate(['login']));
+}
+
+  ngOnInit(): void {
+  }
+
+}
